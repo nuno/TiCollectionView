c47eeaea10a00a79b323e0f48e5a5c377c6a6373
Ayo Adesugba <ayoadesugba@gmail.com>
2015-01-28 19:46:56 -0500

    Add Custom Reusable Views for header/footer

---
+++ iphone/DeMarcelpociotCollectionviewHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  DeMarcelpociotCollectionviewHeaderViewCollectionReusableView.h
+//  TiCollectionView
+//
+//  Created by Ayorinde Adesugba on 1/23/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DeMarcelpociotCollectionviewHeaderView : UICollectionReusableView
+
+@end
---
+++ iphone/DeMarcelpociotCollectionviewHeaderView.m
@@ -0,0 +1,16 @@
+//
+//  DeMarcelpociotCollectionviewHeaderViewCollectionReusableView.m
+//  TiCollectionView
+//
+//  Created by Ayorinde Adesugba on 1/23/15.
+//
+//
+
+#import "DeMarcelpociotCollectionviewHeaderView.h"
+
+@implementation DeMarcelpociotCollectionviewHeaderView
+
+- (void)prepareForReuse {
+    [super prepareForReuse];
+}
+@end
